84b3ba1be7bff7b1b313b43ae22c8f093a3cf447
GuillemMV <gmagrina1@institutjaumehuguet.cat>
2023-01-17 17:21:15 +0100

    Add mecanica preguntes.

--- app/src/main/java/com/example/orguevalls/MainActivity.java
+++ app/src/main/java/com/example/orguevalls/MainActivity.java
@@ -1,15 +1,83 @@
 package com.example.orguevalls;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView textRespostesCorrectes;
+    String opcioSeleccionada = "";
+    TextView pregunta;
+    ImageView imatgePregunta;
+    Button botoSubmit;
+    ImageView topLeft, topRight, middleLeft, middleRight, bottomLeft, bottomRight;
+    int puntuacio = 0;
+    int indexPreguntaActual = 0;
+    String respostaSeleccionada = "";
+    int numPreguntesTotals = 6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.pregunta);
         getSupportActionBar().hide();
+
+
+        pregunta = findViewById(R.id.pregunta);
+        imatgePregunta = findViewById(R.id.img);
+        botoSubmit = findViewById(R.id.ButtonSeguent);
+        
+        topLeft = findViewById(R.id.top_left);
+        topRight = findViewById(R.id.top_right);
+        middleLeft = findViewById(R.id.middle_left);
+        middleRight = findViewById(R.id.middle_right);
+        bottomLeft = findViewById(R.id.bottom_left);
+        bottomRight = findViewById(R.id.bottom_right);
+        
+        
+        topLeft.setOnClickListener(this);
+        topRight.setOnClickListener(this);
+        middleLeft.setOnClickListener(this);
+        middleRight.setOnClickListener(this);
+        bottomLeft.setOnClickListener(this);
+        bottomRight.setOnClickListener(this);
+
+        //textRespostesCorrectes.setText("Respostes correctes: "+puntuacio);
+        carregaNovaPregunta();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        ImageView imatgeSeleccionada = (ImageView) view;
+
+        Toast.makeText(getApplicationContext(),imatgeSeleccionada.toString(),Toast.LENGTH_SHORT).show();
+
+        if(opcioSeleccionada.equals(Opcions.correctAnswers[indexPreguntaActual])){
+            puntuacio++;
+            indexPreguntaActual++;
+            carregaNovaPregunta();
+        }
+    }
+
+
+    private void carregaNovaPregunta() {
+        if(indexPreguntaActual == numPreguntesTotals){
+            acabaTest();
+            return;
+        }
+
+        pregunta.setText(Opcions.question[indexPreguntaActual]);
+        imatgePregunta.setImageResource(Opcions.images[indexPreguntaActual]);
+
+    }
+
+    private void acabaTest() {
     }
 }
---
+++ app/src/main/java/com/example/orguevalls/Opcions.java
@@ -0,0 +1,24 @@
+package com.example.orguevalls;
+
+public class Opcions {
+
+    public static String question[] ={
+            "Busqueu aquest carreu i situeu-lo al plànol.",
+            "Segons la imatge, on creieu que estava situat l’orgue anterior? Situeu-lo al plànol.",
+    };
+
+    public static int choices[][] = {
+            {R.id.top_left,R.id.top_right,R.id.middle_left, R.id.middle_right, R.id.bottom_left, R.id.bottom_right},
+            {R.id.top_left,R.id.top_right,R.id.middle_left, R.id.middle_right, R.id.bottom_left, R.id.bottom_right}
+    };
+
+    public static int correctAnswers[] = {
+            R.id.bottom_left,
+            R.id.top_right
+    };
+
+    public static int[] images = {
+            R.drawable.pregunta1,
+            R.drawable.pregunta2,
+    };
+}
