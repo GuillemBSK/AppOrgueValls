38f54bcd41d6742f94e661eafa0d40c03e5dc64b
Rau18 <RaulSG188@gmail.com>
2023-03-21 15:35:56 +0100

    Image button acabado,

--- app/src/main/java/com/example/orguevalls/CantatesOrgue.java
+++ app/src/main/java/com/example/orguevalls/CantatesOrgue.java
@@ -4,9 +4,12 @@ import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class CantatesOrgue extends AppCompatActivity {
+
+    public int clicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +28,29 @@ public class CantatesOrgue extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                soundpool.play(soundId, 1, 1, 1, 0, 1);
+                if (clicked == 0) {
+                    soundpool.play(soundId, 1, 1, 1, 0, 1);
+                    play.setBackground(getResources().getDrawable(R.drawable.volume_on));
+                    clicked = 1;
+                } else if (clicked == 1) {
+                    soundpool.stop(soundId);
+                    play.setBackground(getResources().getDrawable(R.drawable.volume_off));
+                    clicked = 0;
+                }
             }
         });
+
+        Button btnEnrere = findViewById(R.id.btn_enrere_cantates);
+        btnEnrere.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent switchIntent = new Intent(CantatesOrgue.this, MainActivity.class);
+                startActivity(switchIntent);
+            }
+        });
+
+
     }
 
 
